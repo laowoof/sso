38e70ed01bb53f87ea3c6f8d4da4e94e329bd4cf
u46690 <u46690@eea4cbdc-65f9-48cd-8eb9-a3b7e569899f.prvt.dyno.rt.heroku.com>
2018-04-12 05:39:14 +0000

    Initial project layout

---
+++ src/test/java/org/apereo/cas/CasWarOverlayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.apereo.cas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CasWarOverlayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
